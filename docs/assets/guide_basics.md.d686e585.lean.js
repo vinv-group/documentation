28e0d6f67db1e45816f7072c0aade4eeced11d57
b-lack <gerrit.balindt@b-lack.io>
2023-02-06 19:19:02 +0100

    remove gh-deploy

---
+++ docs/assets/guide_basics.md.d686e585.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as t,a as s}from"./app.dc250873.js";const v=JSON.parse('{"title":"vinv-Basics","description":"","frontmatter":{},"headers":[],"relativePath":"guide/basics.md","lastUpdated":1668718410000}'),i={name:"guide/basics.md"},n=s("",4),c=[n];function o(r,d,l,h,_,f){return a(),t("div",null,c)}const u=e(i,[["render",o]]);export{v as __pageData,u as default};
---
+++ docs/assets/guide_envelope.md.c7dc6b83.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,a as p}from"./app.dc250873.js";const u=JSON.parse('{"title":"Envelope","description":"","frontmatter":{},"headers":[],"relativePath":"guide/envelope.md","lastUpdated":1668718410000}'),l={name:"guide/envelope.md"},o=p("",3),e=[o];function t(F,E,r,c,y,C){return n(),a("div",null,e)}const D=s(l,[["render",t]]);export{u as __pageData,D as default};
